b69d5bd4b1858704a91214de4c26c72e5fa8c220
qwerewqwerew <qwerew0@gmail.com>
2022-07-16 21:53:59 +0900

    상품업로드구현완료

--- server.js
+++ server.js
@@ -2,15 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+	storage: multer.diskStorage({
+		destination: function (req, file, cb) {
+			cb(null, "uploads");
+		},
+		filename: function (req, file, cb) {
+			cb(null, file.originalname)
+		},
+	}),
+});
 const port = 8080;
 
 app.use(express.json());
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
 
 app.get("/products", function (req, res) {
 	models.Product.findAll({
 		order: [["createdAt", "DESC"]],
-		attributes:["id","name","price","seller","imageUrl","createdAt"]
+		attributes: ["id", "name", "price", "seller", "imageUrl", "createdAt"],
 	})
 		.then((result) => {
 			res.send({
@@ -19,7 +31,7 @@ app.get("/products", function (req, res) {
 		})
 		.catch((err) => {
 			console.error(err);
-			res.send("에러발생");
+			res.status(400).send("에러발생");
 		});
 });
 
@@ -36,14 +48,14 @@ app.get("/products/:id", function (req, res) {
 		})
 		.catch((err) => {
 			console.error(err);
-			res.send("상품조회시 에러가 발생했습니다.");
+			res.status(400).send("상품조회시 에러가 발생했습니다.");
 		});
 });
 
 app.post("/products", function (req, res) {
 	const body = req.body;
-	const { name, description, price, seller } = body;
-	if (!name || !description || !price || !seller) {
+	const { name, description, price, seller, imageUrl } = body;
+	if (!name || !description || !price || !seller || imageUrl) {
 		res.send("모든 필드를 입력해주세요");
 	}
 	models.Product.create({
@@ -51,14 +63,23 @@ app.post("/products", function (req, res) {
 		description,
 		price,
 		seller,
+		imageUrl
 	})
 		.then((result) => {
 			console.log("상품생성결과:", result);
 		})
 		.catch((err) => {
 			console.error(err);
-			res.send("상품업로드에 문제가 발생했습니다");
+			res.status(400).send("상품업로드에 문제가 발생했습니다");
 		});
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+	const file = req.file;
+	console.log(file);
+	res.send({
+		imageUrl: file.path,
+	});
 });
 
 app.listen(port, () => {

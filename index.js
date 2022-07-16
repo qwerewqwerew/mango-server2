927b387f5eb0d43f8e9af1224055a0a4178b5f29
qwerewqwerew <qwerew0@gmail.com>
2022-07-16 23:49:25 +0900

    heroku배포준비

--- index.js
+++ index.js
@@ -1,29 +1,109 @@
-var http = require("http");
-var hostname = "127.0.0.1";
-var port = "8080"; //서버포트
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+	storage: multer.diskStorage({
+		destination: function (req, file, cb) {
+			cb(null, "uploads");
+		},
+		filename: function (req, file, cb) {
+			cb(null, file.originalname);
+		},
+	}),
+});
+const port = process.env.PORT || 8080;
 
-//서버생성 (req 요청, res 응답)
-const server = http.createServer((req, res) => {
-	const path = req.url;
-	const method = req.method;
-	if (path === "/products") {
-		if (method === "GET") {
-			res.writeHead(200, {
-				"Content-Type": "application/json",
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+app.get("/products", function (req, res) {
+	models.Product.findAll({
+		order: [["createdAt", "DESC"]],
+		attributes: ["id", "name", "price", "seller", "imageUrl", "createdAt"],
+	})
+		.then((result) => {
+			res.send({
+				product: result,
 			});
-			const products = JSON.stringify([
-				{
-					name: "배변패드",
-					price: 50000,
-				},
-			]);
-      res.end(products)
-		}else if(method === "POST"){
-      res.end("생성되었습니다")
-    }
-	}
-  res.end("ByeBye")
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(400).send("에러발생");
+		});
 });
 
-server.listen(port, hostname);
-console.log("mango shop server on");
+app.get("/products/:id", function (req, res) {
+	const params = req.params;
+	const { id } = params;
+	models.Product.findOne({
+		where: {
+			id,
+		},
+	})
+		.then((result) => {
+			res.send({ product: result });
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(400).send("상품조회시 에러가 발생했습니다.");
+		});
+});
+
+app.post("/products", function (req, res) {
+	const body = req.body;
+	const { name, description, price, seller, imageUrl } = body;
+	if (!name || !description || !price || !seller || imageUrl) {
+		res.send("모든 필드를 입력해주세요");
+	}
+	models.Product.create({
+		name,
+		description,
+		price,
+		seller,
+		imageUrl,
+	})
+		.then((result) => {
+			console.log("상품생성결과:", result);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.status(400).send("상품업로드에 문제가 발생했습니다");
+		});
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+	const file = req.file;
+	console.log(file);
+	res.send({
+		imageUrl: file.path,
+	});
+});
+
+app.get("/banners", (req, res) => {
+	models.Banner.findAll({
+		limit:2,
+	}).then((result)=>{
+		res.send({
+			banners:result,
+		})
+	}).catch((error)=>{
+		res.status(500).send('에러가 발생했습니다.',error)
+	})
+});
+
+app.listen(port, () => {
+	console.log("망고샵 서버 실행중");
+	models.sequelize
+		.sync()
+		.then(() => {
+			console.log("DB 연결 성공");
+		})
+		.catch((err) => {
+			console.log("DB 연결 실패");
+			console.error(err);
+			process.exit();
+		});
+});
--- models/banner.js
+++ models/banner.js
@@ -1,11 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
 	const banner = sequelize.define("Banner", {
 		imageUrl: {
-			type: DataTypes.STRING(300),
+			type: dataTypes.STRING(300),
 			allowNull: false,
 		},
     href:{
-      type: DataTypes.STRING(300),
+      type: dataTypes.STRING(300),
 			allowNull: false,
     }
 	});
